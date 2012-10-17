fb83a1f4ad5891c362ee3ea3c0cf55a2163ce426
Erwin Oldenkamp <oldenkamperwin@gmail.com>
2012-10-17 22:15:14 +0200

    Added some more stubs. This is going to be easy peasy

--- mock.py
+++ mock.py
@@ -8,10 +8,20 @@ app.debug = True
 
 @app.route("/tripOffer", methods=['GET'])
 def tripOfferGet():
-	json_data=open('json/get_tripOffers.json')
+	return dumpJsonFile('get_tripOffers.json')
 
+@app.route("/tripOffer", methods=['POST'])
+def tripOfferPost():
+	return dumpJsonFile('get_userById.json')
+
+@app.route("/tripOffer/<tripOfferId>", methods=['GET'])
+def tripOfferGetvar(tripOfferId):
+	return dumpJsonFile('get_tripOfferById.json')
+
+
+def dumpJsonFile(filename):
+	json_data=open('json/'+filename)
 	data = json.load(json_data)
-
 	return json.dumps(data)
 
 if __name__ == "__main__":
